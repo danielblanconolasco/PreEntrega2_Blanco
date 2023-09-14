8c527fe58fd87978e9b419326f85b11037ce18f2
Daniel Blanco <danielblanconolasco@gmail.com>
2023-09-14 01:07:25 -0300

    constructor con bucle de entrada para personas

--- js/script.js
+++ js/script.js
@@ -1,23 +1,72 @@
 let personas = []
-let mascotas = []
 
 class Persona {
     constructor(nombre, edad, sexo, mascotas) {
-        this.nombre  = nombre.toUpperCase()
-        this.edad  = parseFloat(edad)
-        this.sexo = sexo
-        this.mascotas = this.mascotas(mascotas)
-    }
-    mascotas(mascotaIngreso) {
-        let  { nombre, especie } = mascotaIngreso
-        this.mascotaNombre = nombre
-        this.mascotaEspecie = especie.toLowerCase()
+        this.nombre = capitalizar(nombre)
+        this.edad = parseFloat(edad)
+        this.sexo = sexo.toLowerCase()
+        this.mascotas = mascotas
     }
 }
-function bienvenido (){
-    let persona
-    alert("mensaje")
-    nombre = prompt("Como te llamas")
-    persona.push(new persona(nombre, edad, sexo, mascotas))
+
+function obtenerNombre() {
+    let nombre
+    
+    do {
+        nombre = prompt("¿Cómo te llamas?")
+    } while (!nombre && !isNaN(nombre))
+    return  capitalizar(nombre)
 }
-bienvenido ()
+
+let capitalizar = (nombre) => nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase()
+
+
+function obtenerEdad(nombre) {
+    let edad
+
+    do {
+        edad = prompt(`¡Hola ${capitalizar(nombre)}!\n¿Cuál es tu edad?`)
+    } while (isNaN(edad) || edad === "")
+
+    return edad
+}
+
+
+function obtenerSexo(nombre) {
+    let sexo
+
+    do {
+        sexo = prompt(`¿ ${capitalizar(nombre)} cuál es tu sexo?\n\n Ingresa una de las opciones\n- Hombre\no\n- Mujer`).toLowerCase()
+    } while (sexo !== "hombre" && sexo !== "mujer")
+
+    return sexo
+}
+
+function bienvenido() {
+    while (true) {
+        let nombre = obtenerNombre()
+        let edad = obtenerEdad(nombre)
+        let sexo = obtenerSexo(nombre)
+        let mascotas = []
+
+        let numMascotas = prompt("¿Cuántas mascotas tienes?")
+        numMascotas = parseInt(numMascotas)
+
+        for (let i = 0; i < numMascotas; i++) {
+            let mascotaNombre = prompt(`Nombre de tu mascota #${i + 1}`)
+            let mascotaEspecie = prompt(`Especie de tu mascota #${i + 1}`)
+            mascotas.push({ nombre: mascotaNombre, especie: mascotaEspecie })
+        }
+
+        let persona = new Persona(nombre, edad, sexo, mascotas)
+        personas.push(persona)
+
+        let continuar = prompt("¿Deseas agregar otro perfil? (si/no)").toLowerCase()
+        if (continuar !== "si") {
+            break
+        }
+    }
+}
+
+bienvenido()
+console.log(personas)

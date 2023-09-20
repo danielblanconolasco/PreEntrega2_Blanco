b120c013d1ad9003413ae097d2adb250e95bced3
Daniel Blanco <danielblanconolasco@gmail.com>
2023-09-19 21:25:59 -0300

    correciones en logica para entrega 2

--- js/script.js
+++ js/script.js
@@ -11,15 +11,14 @@ class Persona {
 
 function obtenerNombre() {
     let nombre
-    
+
     do {
         nombre = prompt("¿Cómo te llamas?")
     } while (!nombre && !isNaN(nombre))
-    return  capitalizar(nombre)
+    return capitalizar(nombre)
 }
 
 let capitalizar = (nombre) => nombre.charAt(0).toUpperCase() + nombre.slice(1).toLowerCase()
-
 
 function obtenerEdad(nombre) {
     let edad
@@ -31,7 +30,6 @@ function obtenerEdad(nombre) {
     return edad
 }
 
-
 function obtenerSexo(nombre) {
     let sexo
 
@@ -42,31 +40,100 @@ function obtenerSexo(nombre) {
     return sexo
 }
 
-function bienvenido() {
+function crearPersona() {
+    let nombre = obtenerNombre()
+    let edad = obtenerEdad(nombre)
+    let sexo = obtenerSexo(nombre)
+    let mascotas = []
+
+    let numMascotas = prompt("¿Cuántas mascotas tienes?")
+    numMascotas = parseInt(numMascotas)
+
+    for (let i = 0; i < numMascotas; i++) {
+        let mascotaNombre = prompt(`Nombre de tu mascota #${i + 1}`)
+        let mascotaEspecie = prompt(`¿Es un perro o un gato #${i + 1}`)
+        mascotas.push({ nombre: mascotaNombre, especie: mascotaEspecie.toLowerCase() })
+    }
+
+    let persona = new Persona(nombre, edad, sexo, mascotas)
+    personas.push(persona)
+    return persona
+}
+
+let personaElegida = crearPersona()
+
+// Lista de productos
+
+let productos = [
+    { id: 1, nombre: "Comida para perros de 15 Kg", precio: 24990, mascota: "perro" },
+    { id: 2, nombre: "Comida para gatos de 8 kg", precio: 14990, mascota: "gato" },
+    { id: 3, nombre: "Juguetes para mascotas", precio: 6990, mascota: "perro/gato" },
+    { id: 4, nombre: "Collar", precio: 9990, mascota: "perro/gato" },
+]
+
+// Función para mostrar los productos y permitir que la persona elija qué producto comprar
+function comprarProductos(persona) {
+    let carrito = []
+    let total = 0
+
+    let mascotasElegidas = persona.mascotas.map((mascota) => mascota.especie)
+
     while (true) {
-        let nombre = obtenerNombre()
-        let edad = obtenerEdad(nombre)
-        let sexo = obtenerSexo(nombre)
-        let mascotas = []
+        let productosRelevantes = productos.filter((producto) => {
+            let mascotaProducto = producto.mascota.toLowerCase()
+            return mascotasElegidas.includes(mascotaProducto) || mascotaProducto.includes("perro/gato")
+        })
 
-        let numMascotas = prompt("¿Cuántas mascotas tienes?")
-        numMascotas = parseInt(numMascotas)
-
-        for (let i = 0; i < numMascotas; i++) {
-            let mascotaNombre = prompt(`Nombre de tu mascota #${i + 1}`)
-            let mascotaEspecie = prompt(`Especie de tu mascota #${i + 1}`)
-            mascotas.push({ nombre: mascotaNombre, especie: mascotaEspecie })
-        }
-
-        let persona = new Persona(nombre, edad, sexo, mascotas)
-        personas.push(persona)
-
-        let continuar = prompt("¿Deseas agregar otro perfil? (si/no)").toLowerCase()
-        if (continuar !== "si") {
+        if (productosRelevantes.length === 0) {
+            alert("No hay productos relevantes para tus mascotas.")
             break
         }
+
+        let mensajeProductos = "\nProductos disponibles:\n"
+        for (let producto of productosRelevantes) {
+            mensajeProductos += `${producto.id}. ${producto.nombre} - Precio: $${producto.precio}\n`
+        }
+
+        mensajeProductos += "0. Finalizar compra"
+        let opcion = prompt(mensajeProductos)
+        opcion = parseInt(opcion)
+
+        if (opcion === 0) {
+            break
+        }
+
+        let productoElegido = productosRelevantes.find((producto) => producto.id === opcion)
+
+        if (productoElegido) {
+            let carritoItem = carrito.find((item) => item.id === productoElegido.id)
+
+            if (carritoItem) {
+                carritoItem.cantidad += 1
+            } else {
+                carritoItem = { ...productoElegido, cantidad: 1 }
+                carrito.push(carritoItem)
+            }
+
+            total += productoElegido.precio
+            alert(`Has añadido ${productoElegido.nombre} al carrito.`)
+        } else {
+            alert("El producto seleccionado no existe o no es relevante para tus mascotas.")
+        }
+    }
+
+    if (carrito.length > 0) {
+        let resumenCompra = "Resumen de la compra:\n\n"
+        for (let item of carrito) {
+            resumenCompra += `Producto: ${item.nombre}\nCantidad: ${item.cantidad}\nPrecio: $${item.precio * item.cantidad}\n\n`
+        }
+        resumenCompra += `Total a pagar: $${total}`
+
+        alert(resumenCompra)
+    } else {
+        alert("No has seleccionado ningún producto en tu carrito.")
     }
 }
 
-bienvenido()
-console.log(personas)
+if (personaElegida) {
+    comprarProductos(personaElegida)
+}
